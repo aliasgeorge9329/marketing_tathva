9c830dc6f78bb77a51c978f2154d3e0159efb1fc
ALIAS GEORGE <bps@ALIAS-MacBook-Pro.local>
2021-12-20 10:39:25 +0530

    added photos

--- js/script.js
+++ js/script.js
@@ -39,3 +39,21 @@ $(window).resize(function () {
   if (window.innerWidth <= 600) navbar.addClass("fixed-top");
   else navbar.removeClass("fixed-top");
 });
+// ===== Scroll to Top ====
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 50) {
+    // If page is scrolled more than 50px
+    $("#return-to-top").fadeIn(200); // Fade in the arrow
+  } else {
+    $("#return-to-top").fadeOut(200); // Else fade out the arrow
+  }
+});
+$("#return-to-top").click(function () {
+  // When arrow is clicked
+  $("body,html").animate(
+    {
+      scrollTop: 0, // Scroll to top of body
+    },
+    500
+  );
+});
